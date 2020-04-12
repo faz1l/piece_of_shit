47a96a7a7e914e8d040ae723046578d7db3e391d
faz1l <gl1t428@gmail.com>
2020-04-12 14:03:33 +0300

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/piece_of_shit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.piece_of_shit
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
